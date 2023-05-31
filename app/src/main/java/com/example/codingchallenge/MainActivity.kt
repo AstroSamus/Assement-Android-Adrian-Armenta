811ac16f20dd9db43340652b7879cdd799d3343e
Adrian Armenta <Adrian.Armenta@Honeywell.com>
2023-05-31 01:51:22 -0600

    Feature to request permissions to te user. Currently is requesting permissions to use the camera

--- app/src/main/java/com/example/codingchallenge/MainActivity.kt
+++ app/src/main/java/com/example/codingchallenge/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.codingchallenge
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +17,48 @@ class MainActivity : AppCompatActivity() {
 
         //Remove the app title bar
         supportActionBar?.hide()
+
+        //Request permisions
+        if (!arePermissionsGranted()) {
+            grantPermissionsActivityResult.launch(REQUIRED_PERMISSIONS)
+        }
     }
+
+    private fun arePermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            applicationContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private val grantPermissionsActivityResult =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        )
+        { permissions ->
+            // Handle Permission granted/rejected
+            var permissionGranted = true
+            permissions.entries.forEach {
+                if (it.key in REQUIRED_PERMISSIONS && it.value == false)
+                    permissionGranted = false
+            }
+            if (!permissionGranted) {
+                Toast.makeText(
+                    baseContext,
+                    R.string.app_will_not_work_permissions,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    companion object {
+        val REQUIRED_PERMISSIONS = mutableListOf (
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
+
 }
