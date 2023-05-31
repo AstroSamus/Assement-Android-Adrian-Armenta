750eeec3f689fb9f6e2b92f8b9178f46c8b28fa6
Adrian Armenta <Adrian.Armenta@Honeywell.com>
2023-05-31 13:35:29 -0600

    Renamed Item data and placing it into a new folder called database. Now the primaryKey is not a UUID but an autogenerated Int, also, the image is a string, instead of URI because we cannot store URI objects in a db

--- app/src/main/java/com/example/codingchallenge/database/Item.kt
+++ app/src/main/java/com/example/codingchallenge/database/Item.kt
@@ -1,15 +1,19 @@
-package com.example.codingchallenge.data
+package com.example.codingchallenge.database
 
 import android.content.Context
 import android.net.Uri
 import androidx.core.content.ContextCompat
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.codingchallenge.R
 import java.net.URI
 import java.util.*
-
+@Entity
 data class Item(
     var name: String,
     var colorCode: String,
-    var image: Uri,
-    var id: UUID = UUID.randomUUID()
-)
+    var image: String,
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+}
