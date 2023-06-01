02b588305e50fc083dc45094dca816580b76e3f3
Adrian Armenta <Adrian.Armenta@Honeywell.com>
2023-06-01 17:14:13 -0600

    Recycler view of the items list stored in db has been implemented

--- app/src/main/java/com/example/codingchallenge/ItemsMenuFragment.kt
+++ app/src/main/java/com/example/codingchallenge/ItemsMenuFragment.kt
@@ -1,17 +1,22 @@
 package com.example.codingchallenge
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.codingchallenge.adapter.ItemElementAdapter
+import com.example.codingchallenge.database.ItemDatabase
 import com.example.codingchallenge.utils.CoroutineFragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.launch
 
 class ItemsMenuFragment : CoroutineFragment() {
 
     private lateinit var buttonCreateItem: FloatingActionButton
+    private lateinit var itemListContainer: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,10 +30,22 @@ class ItemsMenuFragment : CoroutineFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         buttonCreateItem = view.findViewById(R.id.buttonCreateItem)
+        itemListContainer = view.findViewById(R.id.itemListContainer)
 
         buttonCreateItem.setOnClickListener {
             val action = ItemsMenuFragmentDirections.actionItemsMenuFragmentToCreateItemsFragment()
             findNavController().navigate(action)
+        }
+
+        itemListContainer.apply {
+            layoutManager = LinearLayoutManager(context)
+        }
+
+        launch {
+            context?.let {
+                val itemsDataset = ItemDatabase(it).getItemDao().getAll()
+                itemListContainer.adapter = ItemElementAdapter(itemsDataset)
+            }
         }
     }
 }
